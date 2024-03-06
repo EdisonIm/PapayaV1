7096f6b5bb088606064f08084929eb9bedc4852e
bs <alicparadigm@gmail.com>
2024-03-06 12:45:35 +0900

    f-v2-app_Integration_metro.config.js_beforeUpdate_noError

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen'; 
 import store from './src/store';
 import AppInner from './AppInner';
 import {navigationRef} from './src/utils/NavigationService';
@@ -8,16 +11,33 @@ import {RefreshProvider} from './src/utils/RefreshContext';
 import {ThemeProvider} from 'styled-components/native';
 import theme from './theme';
 
+const queryClient = new QueryClient();
+
 function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter, // Colors.white로 변경 가능
+    flex: 1,
+  };
+
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer ref={navigationRef}>
-          <RefreshProvider>
-            <AppInner />
-          </RefreshProvider>
-        </NavigationContainer>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <NavigationContainer ref={navigationRef}>
+            <RefreshProvider>
+              <SafeAreaView style={backgroundStyle}>
+                <StatusBar
+                  barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                  backgroundColor={backgroundStyle.backgroundColor}
+                />
+                <AppInner />
+              </SafeAreaView>
+            </RefreshProvider>
+          </NavigationContainer>
+        </ThemeProvider>
+      </QueryClientProvider>
     </Provider>
   );
 }
